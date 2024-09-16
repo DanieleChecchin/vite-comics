ba309a1bfede49e7d0af8d0ce4f1cdb711e6febf
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-16 15:53:51 +0200

    AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,10 +10,22 @@ export default{
 
 <template>
     <main>
-        <h1>main</h1>
+        <p class="container">--> Content goes here <--</p>
     </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../styles/general.scss" as *;
+
+main{
+    background-color: black;
+    
+    p{
+        color: white;
+        padding: 30px 0;
+        font-size: 20px;
+        font-weight: bold;
+    }
+}
     
 </style>
