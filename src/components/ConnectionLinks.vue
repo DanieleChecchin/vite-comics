b1205fb2eb60a58de7d258a626b5726af9db5e9f
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-16 16:16:27 +0200

    ConnectionLinks

--- src/components/ConnectionLinks.vue
+++ src/components/ConnectionLinks.vue
@@ -10,10 +10,45 @@ export default{
 
 <template>
     <section>
-        <h1>connection links</h1>
+        <ul class="container">
+            <li> <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics"><span>DIGITAL COMICS</span></li>
+            <li> <img src="../assets/img/buy-comics-merchandise.png" alt="Dc Merchandise"><span>DC MERCHANDISE</span></li>
+            <li> <img src="../assets/img/buy-comics-subscriptions.png" alt="Subscription"><span>SUBSCRIPTION</span></li>
+            <li> <img src="../assets/img/buy-comics-shop-locator.png" alt="Shop Locator"><span>COMIC SHOP LOCATOR</span></li>
+            <li> <img src="../assets/img/buy-dc-power-visa.svg" alt="Power Visa"><span>DC POWER VISA</span></li>
+        </ul>
     </section>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../styles/general.scss" as *;
+
+section{
+    background-color: #0282F9;
+    padding: 40px 0;
+
+    ul{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+
+        li{
+            color: white;
+            display: flex;
+            align-items: center;
+            padding-right: 30px;
+            font-size: 14px;
+            
+            img{
+                width: 40px;
+                height: 50px;
+                margin-right: 10px;
+            }
+            
+        }
+    }
+    
+}
     
 </style>
