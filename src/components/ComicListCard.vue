e596019ade546c2659faf4c58cc03a3958c54683
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-17 15:05:18 +0200

    Aggiunte props

--- src/components/ComicListCard.vue
+++ src/components/ComicListCard.vue
@@ -5,6 +5,24 @@ export default {
         return {
 
         }
+    },
+    props:{
+        comicImage:{
+            type: String,
+            required: true,
+        },
+        comicPrice:{
+            type: Number,
+            required: false
+        },
+        comicName:{
+            type: String,
+            required: true
+        },
+        comicType:{
+            type: String,
+            required: false
+        }
     }
     
 }
